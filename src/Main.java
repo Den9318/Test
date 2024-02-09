7255c13e37db12aab3a62345fbc0bea8c29f8ef4
denisorlov <denis.orlov9318@gmail.com>
2024-02-09 15:25:36 +0100

    C

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -39,6 +41,7 @@ public class Main {
             System.out.println("Number incorrect");
         }
     }
+
 }
 
 
