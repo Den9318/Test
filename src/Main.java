851b8f0b1688725f88efb88d6c41a02216f3ae8f
denisorlov <denis.orlov9318@gmail.com>
2024-02-10 16:10:07 +0100

    C

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//
+
 //        int germansprechen = 58;
 //        int germanschreiben = 59;
 //        int germanlesen = 59;
@@ -41,7 +42,7 @@ public class Main {
 //            System.out.println("Number incorrect");
 //        }
 //    }
-//
+
 //        System.out.println("Size of array: ");
 //        Scanner s = new Scanner(System.in);
 //
@@ -54,15 +55,40 @@ public class Main {
 //            array[i] = s.nextInt();
 //        }
 //        System.out.println(array.length);
+//
+//        System.out.println();
+//        int[] array1 = {1, 2, 3, 4, 5, 6};
+//
+//        for (int i = 9; i > 0; i--) {
+//            for (int j = 1; j < 10; j++) {
+//                System.out.println(i + " * " + j + "= " + i * j);
+//            }
+//            System.out.println();
+//        }
+//    }
+//        int i = 0;
+//        while (i < 5) {
+//            System.out.println(i);
+//            i++;
+//        }
+//        int j = 2;
+//
+//        System.out.println();
+//        do {
+//            System.out.println(j);
+//            j++;
+//        }
+//        while (j < 15);
+//    }
+//}
+        int[][] array = {{1, 2, 3, 4},
+                        {5, 6, 7, 8}};
 
-        System.out.println();
-        int[] array1 = {1, 2, 3, 4, 5, 6};
+        for (int y = 0; y < array.length; y++) {
+            for (int g = 0; g < array[y].length; g++) {
+                System.out.println(array[0][2]);
 
-        for (int i = 9; i > 0; i--) {
-            for (int j = 1; j < 9; j++) {
-                System.out.println(i + " * " + j + "= " + i * j);
             }
-            System.out.println();
         }
     }
 }
