700fa0cc4bb139bfac9d9fb59d6379b1cba80354
denisorlov <denis.orlov9318@gmail.com>
2024-02-26 19:35:44 +0100

    Hw2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println(getTelnumber("+380997668527"));
 //        System.out.println(getPow(3));
 //         System.out.println(getOperation());
 //         char [] array = {'@','#', '%', '^'};
@@ -91,49 +94,76 @@ public class Main {
 //  }
 //
 //}
-        ///1
-        String str = "Hello";
-        String str1 = " World";
+//        System.out.println();
+//        ///1
+//        String str = "Hello";
+//        String str1 = " World";
+//
+//        System.out.println(str + str1);
+//        System.out.println(str.concat(str1));
+//        System.out.println(String.format(str + str1));
+//
+//        ///2
+//        System.out.println();
+//
+//        String con = "Concatenation";
+//
+//        System.out.println(con.substring(5, 7));
+//
+//        ///3
+//
+//        System.out.println();
+//
+//        String name = "Denys";
+//        String secondanme = "Orlov";
+//        String faculty = "Physic";
+//
+//
+//        String sf1 = String.format("Name is: %s", name);
+//        String sf2 = String.format("Second name is: %s", secondanme);
+//        String sf3 = String.format("value is: %s", faculty);
+//
+//        System.out.println(sf1);
+//        System.out.println(sf2);
+//        System.out.println(sf3);
+//
+//        System.out.println();
 
-        System.out.println(str + str1);
-        System.out.println(str.concat(str1));
-        System.out.println(String.format(str + str1));
-
-        ///2
-        System.out.println();
-
-        String con = "Concatenation";
-
-        System.out.println(con.substring(5, 7));
-
-        ///3
-
-        System.out.println();
-
-        String name = "Denys";
-        String secondanme = "Orlov";
-        String faculty = "Physic";
+//        int[] array = {1, 2, 30, 112, 117};
+//        int max = array [0];
+//
+//        for (int i = 1; i < array.length; i++) {
+//            if (array[i] > max) {
+//                max = array[i];
 
 
-        String sf1 = String.format("Name is: %s", name);
-        String sf2 = String.format("Second name is: %s", secondanme);
-        String sf3 = String.format("value is: %s", faculty);
+        ///////////4
+    }
 
-        System.out.println(sf1);
-        System.out.println(sf2);
-        System.out.println(sf3);
+    public static String getTelnumber(String number) {
 
-          System.out.println();
+        String example = ("^\\+[\\(\\-]?(\\d[\\(\\)\\-]?){11}\\d$");
+        String correctnumber = "+380997668527";
+        correctnumber = number;
 
-        int[] array = {1, 2, 30, 112, 117};
-        int max = array [0];
 
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] > max) {
-                max = array[i];
-            }
+        if (correctnumber == example) {
+
+            Pattern patern = Pattern.compile("(\"^\\\\+[\\\\(\\\\-]?(\\\\d[\\\\(\\\\)\\\\-]?){11}\\\\d$\")");
+            Matcher matcher = patern.matcher(number);
+            System.out.println(matcher.find());
+            System.out.println("Correct");
+
+        } else if (number.length() == 13) {
+            System.out.println("Accept");
+
+        } else {
+            System.out.println("Wrong");
 
         }
-            System.out.println(max);
+        return correctnumber;
     }
 }
+
+//            System.out.println(max);
+//    }
